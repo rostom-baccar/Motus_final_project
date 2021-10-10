918e35384b099843e7e6e89be01b3533917b6b14
Rostom Baccar <81230614+98rostom@users.noreply.github.com>
2021-10-10 02:03:29 +0200

    added main class

--- src/Motus_class.java
+++ src/Motus_class.java
@@ -45,7 +45,7 @@ public class Motus_class {
 		} while ((l!=7) || (t==false));  //2 conditions pour que le mot soit accepté: longueur=7 et existence du mot dans le dictionnaire
 		return ch;                       //la fonction readFile retourne le mot proposé
 	}
-	
+
 	public static boolean verif(char c, String ch2) { //la fonction verif retourne false si un caractère donné n'est pas dans 
 		boolean t;                                    //une chaîne donnée, et retourne true si le caractère est bien dans la chaîne
 		int i;
@@ -79,8 +79,8 @@ public class Motus_class {
 		}
 		return (ch2);
 	}	
-	
-	
+
+
 	public static String traitement2(String ch, String ch1, String ch2){  //traitement 2 concerne les lettres mal placées
 		int i,j;
 		boolean t;
@@ -100,5 +100,64 @@ public class Motus_class {
 			}
 		}
 		return (ch2);
-	}	
+	}
+
+	public static void main(String[] args){                
+		System.out.println("BIENVENUE AU JEU MOTUS!");
+		System.out.println("Vous avez 6 essais pour essayer de deviner le mot a 7 lettres tire au hasard");
+		System.out.println();
+		String resp;
+		boolean t1;
+		do {
+			String ch,ch1,ch2;     //ch1 est le mot tiré au harard, ch2 est le mot proposé qui sera
+			ch="";                 //mis à jour après traitement(coloriage des lettres/version beta: min et maj)
+			int i,c; 
+			boolean t;              
+			c=0;      
+			i = (int)(Math.random()*2888);                     //i nombre au hadard entre 0 et 2887 (nombre de mots à 7 lettres)
+			ch1=dictionnaire[i].toUpperCase();                   //ch1 un mot tiré au hasard (selon i) parmi les mots à 7 lettres 
+			ch2=ch1.charAt(0)+"------";                          //on donne la 1ere lettre à l'utilisateur
+			System.out.println(ch2);
+			do {
+
+				c=c+1;
+				ch=readFile(ch);                    //saisie de ch: readFile est une fonciton de saisie et de vérification du mot proposé ch
+
+				if (ch.charAt(0)!=ch2.charAt(0)) { //si la 1ère lettre donnée n'est pas respectée, celle-ci est remplacée par un tiret
+					ch2="-------";
+				}    
+				ch2=traitement1(ch,ch1,ch2); //colorie en rouge les lettres bien placées (version beta: majuscule)
+				ch2=traitement2(ch,ch1,ch2); //colorie en jaune les lettres mal placées (version beta: minuscule), Remarque: traitement 2 utilise verif
+
+
+				//POUR DESACTIVER LA FONCTIONNALITE DE L'APPARTENANCE DU MOT PROPOSE AU DICTIONNAIRE AFIN DE FACILITER LES TESTS, 
+				//IL FAUT AFFECTER t A TRUE JUSTE AVANT LA FIN DE LA BOUCLE WHILE DANS LA FONCITON readFile
+
+				t=ch.equals(ch1);  //comparaison du mot proposé avec le mot à deviner: stockée dans la variable booléenne t
+				if (t==false) {
+
+					System.out.println(ch2);	 //remarque: l'affichage de ch2 dans un 1er temps avant la réinitialisation permet au joueur
+					//de connaitre son avancement quant au devinement du mot
+
+					ch2=ch1.charAt(0)+"------";
+
+					System.out.println("Essais restants: "+(6-c));
+				} 
+			} while ( (c!=6) && (t==false) );
+
+			if (t==true) {
+				System.out.println("Vous avez gagne!");
+			}
+			else {
+				System.out.println("Vous avez perdu!");
+				System.out.println("Le mot a deviner etait "+ch1);
+			}
+
+			System.out.println("Voulez-vous rejouer?");
+			resp=Lire.S().toUpperCase();
+			t1=resp.equals("OUI");
+		}while (t1==true);
+
+	} 
+
 }
